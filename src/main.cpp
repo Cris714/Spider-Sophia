6040152a21a6ef39dc41d1255eec7de93255f9b9
CRISTIAN ALFONSO CARDENAS MORALES <calfcmol@gmail.com>
2024-05-17 16:43:05 -0400

    servo test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
+// #include <Arduino.h>
+
+// // put function declarations here:
+// int myFunction(int, int);
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int pinServo = 18; // Pin GPIO al que está conectado el servo
+Servo miServo;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  miServo.attach(pinServo);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
+  miServo.write(0);    // Mover el servo a 0 grados
+  delay(1000);         // Esperar 1 segundo
+  miServo.write(90);   // Mover el servo a 90 grados
+  delay(1000);         // Esperar 1 segundo
+  miServo.write(180);  // Mover el servo a 180 grados
+  delay(1000);         // Esperar 1 segundo
 }
