a7541562fef44638e5c2bb1431786c4a43d26abd
kris <calfcmol@gmail.com>
2024-05-21 12:13:04 -0400

    añade microros subscriber funcional

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,86 @@
-// #include <Arduino.h>
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
-
-#include <Arduino.h>
 #include <ESP32Servo.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/int32.h>
+
+#include <micro_ros_platformio.h>
+
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 msg;
 
 const int pinServo = 18; // Pin GPIO al que está conectado el servo
 Servo miServo;
 
-void setup() 
+void subscription_callback(const void * msgin)
 {
-  miServo.attach(pinServo);
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+	printf("Received: %d\n", msg->data);
+
+  int value = (int)msg->data;
+  miServo.write(value);
 }
 
-void loop() 
+void setup()
 {
-  miServo.write(0);    // Mover el servo a 0 grados
-  delay(1000);         // Esperar 1 segundo
-  miServo.write(90);   // Mover el servo a 90 grados
-  delay(1000);         // Esperar 1 segundo
-  miServo.write(180);  // Mover el servo a 180 grados
-  delay(1000);         // Esperar 1 segundo
+  IPAddress agent_ip(192, 168, 1, 18);
+  size_t agent_port = 8888;
+  char ssid[] = "CRISTIAN";
+  char psk[] = "95988458";
+
+  set_microros_wifi_transports(ssid, psk, agent_ip, agent_port);
+
+  miServo.attach(pinServo);
+
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "int32_subscriber_rclc", "", &support));
+
+	// create subscriber
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"/microROS/int32_subscriber"));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
+	while(1){
+			rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+			usleep(100000);
+	}
+
+	// free resources
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_node_fini(&node));
+	
+	vTaskDelete(NULL);
+}
+
+void loop()
+{
+
 }
