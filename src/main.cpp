021429621e6e9d55209075603df9a2e9cc8d59b8
isidora hernández <isidora.hernandez@alumnos.uach.cl>
2024-05-22 19:39:50 -0400

    connection wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,90 @@
 #include <ESP32Servo.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string>
 
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
-#include <std_msgs/msg/int32.h>
-
-#include <micro_ros_platformio.h>
-
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-
-#ifdef ESP_PLATFORM
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#endif
-
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
-
-rcl_subscription_t subscriber;
-std_msgs__msg__Int32 msg;
+using namespace std;
 
 const int pinServo = 18; // Pin GPIO al que está conectado el servo
 Servo miServo;
+int currentAngle = 0;
+int speed = 5;
 
-void subscription_callback(const void * msgin)
-{
-	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-	printf("Received: %d\n", msg->data);
+const char* ssid = "Wifi_14000";
+const char* pswd = "wifi14000";
+const int udpPort = 3000;  // Puerto UDP
+WiFiUDP udp;
 
-  int value = (int)msg->data;
-  miServo.write(value);
-}
+void receive_packet();
+void setDefault();
+void moveRight();
+void moveLeft();
 
 void setup()
 {
-  IPAddress agent_ip(192, 168, 1, 18);
-  size_t agent_port = 8888;
-  char ssid[] = "CRISTIAN";
-  char psk[] = "95988458";
+  Serial.begin(115200);
+  delay(500);
 
-  set_microros_wifi_transports(ssid, psk, agent_ip, agent_port);
+  WiFi.begin(ssid, pswd);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  udp.begin(udpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), udpPort);
 
   miServo.attach(pinServo);
-
-	rcl_allocator_t allocator = rcl_get_default_allocator();
-	rclc_support_t support;
-
-	// create init_options
-	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-	// create node
-	rcl_node_t node;
-	RCCHECK(rclc_node_init_default(&node, "int32_subscriber_rclc", "", &support));
-
-	// create subscriber
-	RCCHECK(rclc_subscription_init_default(
-		&subscriber,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"/microROS/int32_subscriber"));
-
-	// create executor
-	rclc_executor_t executor;
-	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
-
-	while(1){
-			rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-			usleep(100000);
-	}
-
-	// free resources
-	RCCHECK(rcl_subscription_fini(&subscriber, &node));
-	RCCHECK(rcl_node_fini(&node));
-	
-	vTaskDelete(NULL);
+  setDefault();
 }
 
 void loop()
 {
+  receive_packet();
+}
 
+void receive_packet() {
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    char packetBuffer[255];
+    IPAddress remoteIp = udp.remoteIP();
+    int len = udp.read(packetBuffer, 255);
+
+    if (len > 0) packetBuffer[len] = 0;
+    Serial.print("From ");    //---|------------------------------------------------|
+    Serial.print(remoteIp);   //---| Use this to find out what IP the text is from. |
+    Serial.print(" : ");      //---|------------------------------------------------|
+    string strCon = packetBuffer;
+
+    Serial.printf("Received packet: %s\n", packetBuffer);
+
+    if(strCon == "mvR") {
+      moveRight();
+    }
+    if(strCon == "mvL") {
+      moveLeft();
+    }
+  }
+}
+
+void setDefault() {
+  miServo.write(0);
+}
+
+void moveRight() {
+  if(currentAngle < 180) {
+    currentAngle += speed;
+    miServo.write(currentAngle);
+  }
+}
+
+void moveLeft() {
+  if(currentAngle > 0) {
+    currentAngle -= speed;
+    miServo.write(currentAngle);
+  }
 }
